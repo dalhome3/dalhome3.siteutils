faf5567ef8b9ccc5c5ff51971a351430c30fecf1
Daniel Ledger <da.ledger01@gmail.com>
2019-08-06 11:48:20 +0100

    Initial Comit

---
+++ SiteUtils/Sitemaps/SitemapItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dalhome3.SiteUtils.Sitemap
+{
+    class SitemapItem
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string ReturnType { get; set; }
+        public string[] Attributes { get; set; }
+    }
+
+    class DistinctSitemapItem : IEqualityComparer<SitemapItem>
+    {
+
+        public bool Equals(SitemapItem x, SitemapItem y)
+        {
+            return x.Action == y.Action &&
+                x.Controller == y.Controller &&
+                x.ReturnType == y.ReturnType;
+        }
+
+        public int GetHashCode(SitemapItem obj)
+        {
+            return obj.Controller.GetHashCode() ^
+                obj.Action.GetHashCode() ^
+                obj.ReturnType.GetHashCode();
+        }
+    }
+}
---
+++ SiteUtils/Sitemaps/Sitemaps.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using dalhome3.SiteUtils.Sitemap;
+using System.Collections.Generic;
+
+namespace dalhome3.SiteUtils
+{
+    public class Sitemaps
+    {
+        public static string Generate(string baseURL, string rootPath)
+        {
+            string output =
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
+            Assembly asm = Assembly.GetCallingAssembly();
+
+            var controllers = asm.GetTypes()
+                .Where(type => typeof(Controller).IsAssignableFrom(type)); //filter controllers
+
+            List<SitemapItem> info = new List<SitemapItem>();
+
+            foreach (var controller in controllers)
+            {
+                var Excluded = controller.IsDefined(typeof(SitemapExcludeAttribute), false);
+
+                info.AddRange(controller.GetMethods()
+                .Where(method => method.IsDefined(typeof(SitemapIncludeAttribute)) || (method.IsPublic && !method.IsDefined(typeof(NonActionAttribute)) && typeof(IActionResult).IsAssignableFrom(method.ReturnType) && !method.IsDefined(typeof(SitemapExcludeAttribute)) && !Excluded))
+                .Select(x => new SitemapItem { Controller = x.DeclaringType.Name.Replace("Controller", "", StringComparison.OrdinalIgnoreCase),
+                    Action = x.Name,
+                    ReturnType = x.ReturnType.Name,
+                    Attributes = x.GetCustomAttributes().Select(a => a.GetType().Name.Replace("Attribute", "")).ToArray()
+                }).ToList());
+            }
+
+            info = info.OrderBy(x => x.Controller).ThenBy(x => x.Action).Distinct(new DistinctSitemapItem()).ToList();
+
+            foreach (var item in info)
+            {
+                output += $"\n\t<url>" +
+                    $"\n\t\t<loc>{baseURL}/{item.Controller}/{item.Action}</loc>";
+                string path = rootPath + $"/Views/{item.Controller}/{item.Action}.cshtml";
+                if (File.Exists(path))
+                {
+                    FileInfo fi = new FileInfo(path);
+                    output += $"\n\t\t<lastmod>{fi.LastWriteTime.ToString("yyyy-MM-dd")}</lastmod>";
+                }
+                output +="\n\t</url>"; ;
+            }
+
+            output += "\n</urlset>";
+
+            return output;
+        }
+
+    }
+}
